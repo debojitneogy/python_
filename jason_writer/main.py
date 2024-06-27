efe224fc20de7f084a6a95b2c9a8789431ce2644
debojitneogy <157280092+debojitneogy@users.noreply.github.com>
2024-06-28 00:50:37 +0530

    added jason creator/reader

---
+++ jason_writer/main.py
@@ -0,0 +1,38 @@
+def create_json(dict,name):
+    with open(f"{name}.json","w") as file_:
+        file_.write("{\n");
+        for i in dict.keys():
+            a = list(dict.keys());
+            if i != a[-1]:
+                file_.write(f'  "{i}": {dict[i]},\n');
+            else:
+                file_.write(f'  "{i}": {dict[i]}\n');
+        file_.write("}");
+
+
+def read_json(path):
+    dict_ = {};
+    f_dict = {};
+    temp = "";
+    with open(path,"r+") as file_:
+        temp = file_.read();
+    temp = temp[1:-2];
+    temp = temp.split(",");
+    for i in temp:
+        t = i.strip();
+        t = t.split(":");
+        a = (t[0].strip());
+        try:
+            b = int(t[1].strip());
+        except:
+            b = t[1].strip();
+        t = {a:b};
+        dict_.update(t);
+    for i in dict_.keys():
+        a = str(i).replace('"','');
+        try:
+            b = int(dict_[i]);
+        except:
+            b = str(dict_[i]).replace('"', '');
+        f_dict.update({a:b});
+    return  f_dict;
