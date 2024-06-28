0c292bae5e39615cef88ca1d3874c58ad14a55cf
debojitneogy <157280092+debojitneogy@users.noreply.github.com>
2024-06-28 11:13:29 +0530

    added jason creator/reader

--- jason_writer/main.py
+++ jason_writer/main.py
@@ -36,3 +36,31 @@ def read_json(path):
             b = str(dict_[i]).replace('"', '');
         f_dict.update({a:b});
     return  f_dict;
+
+def create_list_jason(dict_list,name):
+    for dict in dict_list:
+        with open(f"{name}.json", "a") as file_:
+            file_.write("{\n");
+            for i in dict.keys():
+                a = list(dict.keys());
+                b = dict[i];
+                if isinstance(b,int):
+                    if i != a[-1]:
+                        file_.write(f'  "{i}": {b},\n');
+                    else:
+                        file_.write(f'  "{i}": {b}\n');
+                else:
+                    if i != a[-1]:
+                        file_.write(f'  "{i}": "{b}",\n');
+                    else:
+                        file_.write(f'  "{i}": "{b}"\n');
+            file_.write("}");
+            file_.write("\n");
+
+a = [
+    {"abc":123,123:"abc"},
+    {"abc":1123, 123:"abcc"},
+    {"abc": 11233, 123: "abcec"}
+];
+
+create_list_jason(a,"sample");
