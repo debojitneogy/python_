b1412b76b883dfaea2ee481c5545d2ca13b85ed4
debojitneogy <157280092+debojitneogy@users.noreply.github.com>
2024-06-28 11:57:21 +0530

    added jason creator/reader

--- jason_writer/jason_writer.py
+++ jason_writer/jason_writer.py
@@ -3,13 +3,18 @@ def create_json(dict,name):
         file_.write("{\n");
         for i in dict.keys():
             a = list(dict.keys());
-            if i != a[-1]:
-                file_.write(f'  "{i}": {dict[i]},\n');
+            b = dict[i];
+            if isinstance(b, int):
+                if i != a[-1]:
+                    file_.write(f'       "{i}": {b},\n');
+                else:
+                    file_.write(f'       "{i}": {b}\n');
             else:
-                file_.write(f'  "{i}": {dict[i]}\n');
+                if i != a[-1]:
+                    file_.write(f'       "{i}": "{b}",\n');
+                else:
+                    file_.write(f'       "{i}": "{b}"\n');
         file_.write("}");
-
-#fix string value,
 
 def read_json(path):
     dict_ = {};
@@ -38,7 +43,7 @@ def read_json(path):
         f_dict.update({a:b});
     return  f_dict;
 
-def create_list_jason(dict_list,name):
+def create_list_json(dict_list,name):
     with open(f"{name}.json", "a") as file_:
         file_.write("{\n");
         file_.write(f' "{name}": [\n');
@@ -64,11 +69,3 @@ def create_list_jason(dict_list,name):
                 file_.write("    }\n");
     with open(f"{name}.json", "a") as file_:
         file_.write(" ]\n}");
-
-a = [
-    {"abc":123,123:"abc"},
-    {"abc":1123, 123:"abcc"},
-    {"abc": 11233, 123: "abcec"}
-];
-
-create_list_jason(a,"sample");
