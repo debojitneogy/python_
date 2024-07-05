2e73fab6abcdb0c3cc1d29193acc7f2c2c8e698b
debojitneogy <157280092+debojitneogy@users.noreply.github.com>
2024-07-05 21:52:18 +0530

    list sorting added

--- list_sorting.py
+++ list_sorting.py
@@ -1,36 +1,7 @@
-def sort_list(sorted_list):
-    list = sorted_list;
-    for j in range(len(list)-1):
-        for i in range(len(list)-1):
-            if(list[i] > list[i+1]):
-               a = list[i+1];
-               list[i+1] = list[i];
-               list[i] = a;
-               #print(list);
-    return list;
-
-
-def isspecial_(input_,check):
-    lbool = bool();
-    for i in input_:
-        if(str(i) == str(check)):
-            lbool = True;
-            break;
-        else:
-            lbool = False;
-            continue;
-    return lbool;    
-
-print("press(.) to stop");
-
-main_list = [];
-
-while True:
-    a = input("value: ");
-    if(isspecial_(a,".") == True):
-        break;
-    else:
-        main_list.append(a);
-        continue;
-    
-print(sort_list(main_list),"final");
+def bubble_sort(lis_):
+    n = len(lis_);
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if lis_[j] > lis_[j+1]:
+                lis_[j],lis_[j+1] = lis_[j+1],lis_[j];
+    return lis_;
