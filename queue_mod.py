f9592a03a7b8cddc67dc2f164ca3b69fb8170609
debojitneogy <157280092+debojitneogy@users.noreply.github.com>
2025-03-23 12:18:13 +0530

    fixed queue

--- queue_mod.py
+++ queue_mod.py
@@ -1,18 +1,29 @@
 class queue():
     que = [];
     size = 0;
-    def queue(self,size):
+    f,r = -1,-1;
+    def __init__(self,size):
         self.size = size;
 
     def enqueue(self,obj):
         if len(self.que) < self.size:
             self.que.append(obj);
+            if self.f == -1 and self.r == -1:
+                self.f += 1;
+                self.r += 1;
+            else:
+                self.r += 1;
         else:
             print("Overflow error");
 
     def dequeue(self):
         if len(self.que) > 0:
             return self.que.pop(0);
+            if self.f == 0 and self.r == 0:
+                f -= 1;
+                r -= 1;
+            else:
+                r -= 1;
         else:
             print("underflow error");
 
